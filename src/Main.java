a19c1c0c271bb6fff467b1dddca574949ae7a5bd
Естеу Инкар <esteuinkar@MacBook-Air-Esteu.local>
2025-04-25 23:30:40 +0500

    the beginning of stimulation

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import simulation.SimulationDriver;
+
+public class Main {
+    public static void main(String[] args) {
+        // Запуск симуляции
+        System.out.println("Starting the Airport Tower Simulation...");
+        SimulationDriver.main(args); // Запускает метод main в SimulationDriver для начала симуляции
+    }
+}
