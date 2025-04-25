4d44b5458aa49b8132168fcc9bb84f6eef54d099
Естеу Инкар <esteuinkar@MacBook-Air-Esteu.local>
2025-04-25 23:27:08 +0500

    Helicopter Class

---
+++ src/aircraft/Helicopter.java
@@ -0,0 +1,13 @@
+package aircraft;
+
+public class Helicopter extends Aircraft {
+
+    public Helicopter(String id, int fuelLevel) {
+        super(id, fuelLevel);
+    }
+
+    @Override
+    public void receive(String msg) {
+        System.out.println("Helicopter " + id + " received: " + msg);
+    }
+}
